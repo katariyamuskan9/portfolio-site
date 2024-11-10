3cc8cc64f0df0249788054f2a9ed992d48cdcda9
katariyamuskan9 <51745688+katariyamuskan9@users.noreply.github.com>
2024-11-10 22:18:25 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import {About_text} from "../constants/index"
 const About = () => {
     return (
         <div className='border-b border-neutral-900 pb-4'>
-            <h1 className=' underline my-20 text-center text-4xl'>About<span className='text-neutral-500'>Us</span></h1>
+            <h1 className=' underline my-20 text-center text-4xl'>About<span className='text-neutral-500'>Me</span></h1>
 <div className='flex flex-wrap'>
     <div className='w-full  lg:w-1/2 lg:p-8'>
     <div className='flex justify-center items-center pb-4 '>
